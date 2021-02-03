63332fc633bed73f98b1187d09d936e14970f061
keval gandevia <gandeviakeval05@gmail.com>
2021-02-03 15:26:58 +0530

    sign up view created

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('',views.login,name='login'),
-    path('login_user',views.login_user,name="user_in")
+    path('login_user',views.login_user,name="user_in"),
+    path('signup',views.signup,name="signup")
 ]
--- login/views.py
+++ login/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import customuser
 
 def login(request):
     return render(request,'login/base.html')
@@ -22,4 +25,46 @@ def login_user(request):
         else:
             print("check login or password details")
     return HttpResponse("4040")
-    
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        
+        if check_user_name(username) != None:
+            messages.warning(request,'Username is already taken!!!')
+            return render(request,'login/signup.html')
+        city = request.POST.get('city')
+        country = request.POST.get('country')
+        bio = request.POST.get('bio')
+        password = request.POST.get('password')
+        #check both are same or not
+        c_password = request.POST.get('c_password')
+        #print(username, city, country, bio, password, c_password)
+        user = User.objects.create_user(username, 'lennon@thebeatles.com', password)
+        user.save()
+        custom_user = customuser (city_of_residence = city, country_of_residence = country, bio = bio, user_inher = user)
+        custom_user.save()
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            auth_login(request,user)
+            print("jenil logged in")
+            return redirect('http://localhost:8000/home')
+        else:
+            print("check login or password details")
+        return redirect('http://localhost:8000/home')
+         
+    return render(request,'login/signup.html')
+
+def check_password(password1,password2):
+    if password1 == password2:
+        return True
+    else:
+        return False
+
+
+def check_user_name(username):
+    user = User.objects.filter(username = username)
+    if len(user) == 0:
+        return None
+    return (user)
